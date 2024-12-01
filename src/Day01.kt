7f74bac9e577e75855e3a6f117ddcd10ba63d047
ilya.aliaksandrovich <ilyanerd@proton.me>
2024-12-01 12:48:17 +0100

    day 1 part 2

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,7 +1,7 @@
 import kotlin.math.absoluteValue
 
 fun main() {
-    fun part1(input: List<String>): Int {
+    fun buildLists(input: List<String>): Pair<MutableList<Int>, MutableList<Int>> {
         val list1 = mutableListOf<Int>()
         val list2 = mutableListOf<Int>()
 
@@ -10,7 +10,11 @@ fun main() {
             list1.add(left.toInt())
             list2.add(right.toInt())
         }
+        return list1 to list2
+    }
 
+    fun part1(input: List<String>): Int {
+        val (list1, list2) = buildLists(input)
         val sorted1 = list1.sorted()
         val sorted2 = list2.sorted()
 
@@ -23,7 +27,12 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val (list1, list2) = buildLists(input)
+        val occurrences = list2.groupingBy { number -> number }.eachCount()
+        return list1.sumOf { num ->
+            val occ = occurrences[num] ?: 0
+            num * occ
+        }
     }
 
     // Or read a large test input from the `src/Day01_test.txt` file:
