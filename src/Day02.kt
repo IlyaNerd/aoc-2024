95e9b945af1954460ce44c579c82824f6fe180bb
ilya.aliaksandrovich <ilyanerd@proton.me>
2024-12-02 09:01:43 +0100

    day 2 part 1

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,6 +1,30 @@
 fun main() {
+    fun parse(input: List<String>): List<List<Int>> {
+        return input.map { string ->
+            string.split(" ").map { it.toInt() }
+        }
+    }
+
+    fun List<Int>.isSafe(): Boolean {
+        val isAsc = (this[1] - this[0]) > 0
+        this.forEachIndexed { index, number ->
+            if (index != 0) {
+                val prev = this[index - 1]
+                val diff = number - prev
+                when {
+                    diff == 0 -> return false
+                    diff > 3 || diff < -3 -> return false
+                    diff > 0 && !isAsc -> return false
+                    diff < 0 && isAsc -> return false
+                }
+            }
+        }
+        return true
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        val reports = parse(input)
+        return reports.count { report -> report.isSafe() }
     }
 
     fun part2(input: List<String>): Int {
@@ -10,15 +34,17 @@ fun main() {
     val testInput = readInput("Day02_test")
     val part1Test = part1(testInput)
     println("Part 1 test: $part1Test")
-    check(part1Test == 1) { "part 1 test failed" }
+    check(part1Test == 2) { "part 1 test failed" }
+
+    val input = readInput("Day02")
+
+    print("Part 1: ")
+    part1(input).println()
 
     val part2Test = part1(testInput)
     println("Part 2 test: $part2Test")
     check(part2Test == 1) { "part 2 test failed" }
 
-    val input = readInput("Day02")
-    print("Part 1: ")
-    part1(input).println()
     print("Part 2: ")
     part2(input).println()
 }
