eb4a6adaca80f936d971ca84e0ef9f32c9f416f3
ilya.aliaksandrovich <ilyanerd@proton.me>
2024-12-03 09:08:53 +0100

    day 3 part 1

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,18 +1,24 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val nums = input.joinToString("").let {
+            val regex = Regex("mul\\((\\d+),(\\d+)\\)")
+            regex.findAll(it).map { matchResult ->
+                matchResult.groupValues[1].toInt() to matchResult.groupValues[2].toInt()
+            }.toList()
+        }
+        return nums.sumOf { (l, r) -> l * r }
     }
 
     fun part2(input: List<String>): Int {
         return input.size
     }
 
-    val testInput = readInput("Day00_test")
+    val testInput = readInput("Day03_test")
     val part1Test = part1(testInput)
     println("Part 1 test: $part1Test")
-    check(part1Test == 1) { "part 1 test failed" }
+    check(part1Test == 161) { "part 1 test failed" }
 
-    val input = readInput("Day00")
+    val input = readInput("Day03")
 
     print("Part 1: ")
     part1(input).println()
