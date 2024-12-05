e22b8665209f04a380422201a818a473d3c01f4b
ilya.aliaksandrovich <ilyanerd@proton.me>
2024-12-05 09:48:32 +0100

    day 5 part 2

--- src/Day05.kt
+++ src/Day05.kt
@@ -1,7 +1,8 @@
 fun main() {
     fun parse(input: List<String>): Pair<Set<Pair<Int, Int>>, List<List<Int>>> {
         val splitIndex = input.indexOfFirst { line -> line.contains(",") }
-        val rules = input.take(splitIndex - 1).map { line -> line.split("|").let { it[0].toInt() to it[1].toInt() } }.toSet()
+        val rules =
+            input.take(splitIndex - 1).map { line -> line.split("|").let { it[0].toInt() to it[1].toInt() } }.toSet()
         val numPages = input.drop(splitIndex).map { line -> line.split(",").map(String::toInt) }
         return Pair(rules, numPages)
     }
@@ -20,7 +21,41 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val (rulesSet, numPages) = parse(input)
+
+        return numPages.map { it.toMutableList() }.sumOf { page ->
+            fun fixPage(): Boolean {
+                var incorrect = false
+                for (i in 0..(page.lastIndex - 1)) {
+                    val current = page[i]
+                    val next = page[i + 1]
+                    if (!rulesSet.contains(current to next)) {
+                        incorrect = true
+                        page[i] = next
+                        page[i + 1] = current
+                    }
+                }
+                return incorrect
+            }
+
+            var countPage = false
+
+            while (true) {
+                // would be nice not to go through the page several times 🤔
+                val correct = !fixPage()
+                if (correct) break
+                else {
+                    countPage = true
+                }
+            }
+
+            if (countPage) {
+                val middleNumber = page[page.size / 2]
+                middleNumber
+            } else {
+                0
+            }
+        }
     }
 
     val testInput = readInput("Day05_test")
@@ -35,7 +70,7 @@ fun main() {
 
     val part2Test = part2(testInput)
     println("Part 2 test: $part2Test")
-    check(part2Test == 1) { "part 2 test failed" }
+    check(part2Test == 123) { "part 2 test failed" }
 
     print("Part 2: ")
     part2(input).println()
