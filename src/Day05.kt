311021b82b0de7e8f1fdecfdde9b56c4d556ac69
ilya.aliaksandrovich <ilyanerd@proton.me>
2024-12-05 09:21:02 +0100

    day 5 part 1

--- src/Day05.kt
+++ src/Day05.kt
@@ -1,6 +1,22 @@
 fun main() {
+    fun parse(input: List<String>): Pair<Set<Pair<Int, Int>>, List<List<Int>>> {
+        val splitIndex = input.indexOfFirst { line -> line.contains(",") }
+        val rules = input.take(splitIndex - 1).map { line -> line.split("|").let { it[0].toInt() to it[1].toInt() } }.toSet()
+        val numPages = input.drop(splitIndex).map { line -> line.split(",").map(String::toInt) }
+        return Pair(rules, numPages)
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        val (rulesSet, numPages) = parse(input)
+        return numPages.filter { page ->
+            val correctLine = page.zipWithNext().all { pair ->
+                rulesSet.contains(pair)
+            }
+            correctLine
+        }.sumOf { page ->
+            val middleNumber = page[page.size / 2]
+            middleNumber
+        }
     }
 
     fun part2(input: List<String>): Int {
@@ -10,7 +26,7 @@ fun main() {
     val testInput = readInput("Day05_test")
     val part1Test = part1(testInput)
     println("Part 1 test: $part1Test")
-    check(part1Test == 1) { "part 1 test failed" }
+    check(part1Test == 143) { "part 1 test failed" }
 
     val input = readInput("Day05")
 
