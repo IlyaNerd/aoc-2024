4005d2edc1ab71496736d72fa4c47af3b4090ccf
ilya.aliaksandrovich <ilyanerd@proton.me>
2024-12-06 08:43:20 +0100

    day 6

---
+++ src/Day06.kt
@@ -0,0 +1,26 @@
+fun main() {
+    fun part1(input: List<String>): Int {
+        return input.size
+    }
+
+    fun part2(input: List<String>): Int {
+        return input.size
+    }
+
+    val testInput = readInput("Day06_test")
+    val part1Test = part1(testInput)
+    println("Part 1 test: $part1Test")
+    check(part1Test == 1) { "part 1 test failed" }
+
+    val input = readInput("Day06")
+
+    print("Part 1: ")
+    part1(input).println()
+
+    val part2Test = part2(testInput)
+    println("Part 2 test: $part2Test")
+    check(part2Test == 1) { "part 2 test failed" }
+
+    print("Part 2: ")
+    part2(input).println()
+}
